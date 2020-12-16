7a19a3638225cf6114f9607f15694c6ed10deb64
Vadim <v.dudko93@gmail.com>
2020-12-16 04:28:04 +0300

    feat: add new file graph

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './js/hamburger';
 import './js/table';
 import './js/modal';
+import './js/graph';
 import './assets/css/main.css';
 import './assets/scss/main.scss';
---
+++ src/js/graph.js
@@ -0,0 +1,46 @@
+import Chart from 'chart.js';
+
+const ctx = document.querySelector('#chart').getContext('2d');
+const chartConfig = {
+    type: 'bar',
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        datasets: [
+        ],
+    },
+    options: {
+        title: {
+            display: true,
+            text: 'Number of cases',
+        },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true,
+                    stepSize: 10,
+                },
+                scaleLabel: {
+                    display: true,
+                    labelString: 'millions',
+                },
+            }],
+        },
+        responsive: false,
+    },
+};
+
+const chart = new Chart(ctx, chartConfig);
+const generateRandomTime = () => Math.floor(Math.random() * 20);
+const data = Array(12).fill(0).map(() => generateRandomTime());
+const user = {
+    label: 'cases',
+    data,
+    backgroundColor: '#ffaa00',
+    borderColor: 'transparent',
+    borderWidth: 2,
+    fill: false,
+    barPercentage: 1,
+    categoryPercentage: 0.5,
+};
+chartConfig.data.datasets.push(user);
+chart.update();
