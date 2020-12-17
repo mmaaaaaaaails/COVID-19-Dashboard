b7f2775b5f5923a5e42630671c969b7077364015
ivan-scherbuk <ramz2001ramz@mail.ru>
2020-12-17 23:10:52 +0300

    feat: add event to country

--- src/js/search.js
+++ src/js/search.js
@@ -10,25 +10,37 @@ const searchArea = document.querySelector('.search__input');
 const searchButton = document.querySelector('.search__btn');
 let searchProperty = false;
 
+function chooseCountry() {
+    searchProperty = true;
+    universalList.forEach((item) => item.style.display = 'none');
+    countryDeathList.forEach((item) => item.style.display = 'none');
+    countryRecoveredList.forEach((item) => item.style.display = 'none');
+}
+
 function searchCountry() {
     cross.addEventListener('click', () => {
         document.location.reload();
     });
 
-    // fix bag with index
     searchButton.addEventListener('click', () => {
         for (let i = 0; i < data.Countries.length; i += 1) {
             if (searchArea.value.toLowerCase() === data.Countries[i].Country.toLowerCase()) {
-                searchProperty = true;
-                universalList.forEach((item) => item.style.display = 'none');
-                countryDeathList.forEach((item) => item.style.display = 'none');
-                countryRecoveredList.forEach((item) => item.style.display = 'none');
+                chooseCountry();
                 universalList[i].style.display = 'block';
                 countryDeathList[i].style.display = 'block';
                 countryRecoveredList[i].style.display = 'block';
             }
         }
     });
+
+    for (let i = 0; i < data.Countries.length; i += 1) {
+        universalList[i].addEventListener('click', () => {
+            chooseCountry();
+            universalList[i].style.display = 'block';
+            countryDeathList[i].style.display = 'block';
+            countryRecoveredList[i].style.display = 'block';
+        });
+    }
 }
 
 export { searchCountry, searchProperty };
