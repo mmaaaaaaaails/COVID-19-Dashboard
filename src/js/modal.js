cf52e7b0a57c6e427019a0be7a82b5f92e8dbb8a
Vadim <v.dudko93@gmail.com>
2020-12-23 18:27:37 +0300

    feat: add hover for section death and recovered

--- src/js/modal.js
+++ src/js/modal.js
@@ -8,7 +8,7 @@ import {
     deathsTable,
 } from './table';
 
-import { chart } from './graph';
+import { addGraph } from './graph';
 
 const casesBlockFull = document.querySelector('.cases__block--full');
 const casesFull = document.querySelector('.cases__full');
@@ -27,6 +27,10 @@ const popupClose = document.createElement('span');
 popupClose.className = 'modal__close';
 popupClose.innerHTML = '&times;';
 popup.append(popupClose);
+
+const chart = document.createElement('canvas');
+chart.id = 'myChart';
+popupContent.append(chart);
 
 const modal = document.querySelector('.modal');
 const modalContent = document.querySelector('.modal__content');
@@ -98,6 +102,7 @@ closeModalCross();
 const graphFull = document.querySelector('.graph__full');
 graphFull.addEventListener('click', () => {
     modal.classList.remove('hide');
+    console.log(addGraph());
 });
 
 export {
